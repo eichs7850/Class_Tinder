d405947f907f8e926c87d3798b3df33b8b2ad4c4
damianyoung <74835887+damianyoung@users.noreply.github.com>
2022-10-17 14:17:23 -0400

    Added basic navigation to the UI

--- app/src/main/java/com/example/classtinder/Home.java
+++ app/src/main/java/com/example/classtinder/Home.java
@@ -1,12 +1,52 @@
 package com.example.classtinder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        Button calendarButton = findViewById(R.id.calendarbtn);
+        Button courseSelectionButton = findViewById(R.id.choosebtn);
+        Button reselectProgramButton = findViewById(R.id.repickbtn);
+        Button logOutButton = findViewById(R.id.Logoutbtn);
+
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Calendar.class);
+                startActivity(intent);
+            }
+        });
+
+        courseSelectionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Swipe.class);
+                startActivity(intent);
+            }
+        });
+
+        reselectProgramButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, ProgramSelection.class);
+                startActivity(intent);
+            }
+        });
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/classtinder/MainActivity.java
+++ app/src/main/java/com/example/classtinder/MainActivity.java
@@ -2,13 +2,86 @@ package com.example.classtinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected final static String ACTIVITY_NAME = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.LoginBtn);
+        EditText emailField = findViewById(R.id.email);
+        EditText passwordField = findViewById(R.id.password);
+
+        SharedPreferences sharedPref = getSharedPreferences("DefaultEmail", MODE_PRIVATE);
+        String defaultEmail = sharedPref.getString("DefaultEmail", "email@domain.com");
+        emailField.setText(defaultEmail);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("DefaultEmail", emailField.getText().toString());
+                editor.apply();
+                if(isEmailValid(emailField.getText().toString()) && isPasswordValid(
+                        passwordField.getText().toString())) {
+                    Intent intent = new Intent(MainActivity.this, Home.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity .this, "You did not enter a login email or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDelete()");
+    }
+
+    protected boolean isEmailValid(String email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    protected boolean isPasswordValid(String password) {
+        // This is temporary we should have a check for the actual password
+        // TODO: check the password is correct for the account
+        return password.length() > 0;
     }
 }
