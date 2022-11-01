3510a65d22c8a4546e060c3350d5473ad997d906
Chandler Mayberry <chandlermayberry@live.ca>
2022-10-31 23:01:44 -0400

    List of Changes: -Created the calendar with individual cells for every day of the week -Added strings to string.xml file -Schedule has (almost) working changing timeline -Nearly fully functional in terms of UI -Added another color to color.xml -Made a helper layout for the calendar -Made a drawable object for the calendar layout grid look

--- app/src/main/java/com/example/classtinder/Calendar.java
+++ app/src/main/java/com/example/classtinder/Calendar.java
@@ -1,12 +1,46 @@
 package com.example.classtinder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Calendar extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calander);
+
+        Button calendarBackButton = findViewById(R.id.calendarBackButton);
+        View calendarTimeLine = findViewById(R.id.calendarTimeLine);
+
+        // Set the placement of the calendar time view
+        Date currentTime = new Date();
+        int hour = currentTime.getHours();
+        hour = (hour-7)*60;
+        int min = currentTime.getMinutes();
+        int calendarLineSetValue = hour + min;
+
+        LinearLayout.LayoutParams newParameters = (LinearLayout.LayoutParams) calendarTimeLine.getLayoutParams();
+        newParameters.setMargins(newParameters.leftMargin, calendarLineSetValue, newParameters.rightMargin, newParameters.bottomMargin);
+        calendarTimeLine.setLayoutParams(newParameters);
+
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "The time isnt working properly, work in progress...", Snackbar.LENGTH_LONG);
+        snackbar.show();
+
+        calendarBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Calendar.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
