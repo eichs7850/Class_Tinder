2ced66f1d230a9e0363a317110c58859004d8188
jacobcabral <jacobcabral@gmail.com>
2022-12-03 14:42:50 -0500

    Working google sign in, does nothing currently :^)

--- app/src/main/java/com/example/classtinder/MainActivity.java
+++ app/src/main/java/com/example/classtinder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.classtinder;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,29 +16,56 @@ import com.google.android.gms.auth.api.identity.BeginSignInRequest;
 import com.google.android.gms.auth.api.identity.SignInClient;
 import com.google.android.gms.auth.api.identity.SignInCredential;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     protected final static String ACTIVITY_NAME = "MainActivity";
+    private GoogleSignInClient googleSignInClient;
     FirebaseAuth mAuth;
+    public int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button loginButton = findViewById(R.id.LoginBtn);
+        com.google.android.gms.common.SignInButton googleButton = findViewById(R.id.google);
         EditText emailField = findViewById(R.id.email);
         EditText passwordField = findViewById(R.id.password);
 
         SharedPreferences sharedPref = getSharedPreferences("DefaultEmail", MODE_PRIVATE);
         String defaultEmail = sharedPref.getString("DefaultEmail", "email@domain.com");
         emailField.setText(defaultEmail);
+
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch(view.getId()){
+                    case R.id.google:
+                        signIn();
+                        break;
+                }
+            }
+        });
+
+
 /*
         mAuth = FirebaseAuth.getInstance();*/
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +84,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == RC_SIGN_IN) {
+//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+//
+//            try {
+//                GoogleSignInAccount account = task.getResult(ApiException.class);
+//                firebaseAuthWithGoogle(Objects.requireNonNull(account));
+//            } catch (ApiException e) {
+//                Log.w("hhm", "Google signin failed", e);
+//            }
+//        }
+//    }
+
+    public void signIn(){
+
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
 /*    public void onActivityResult(){
         SignInCredential googleCredential = oneTapClient.getSignInCredentialFromIntent(data);
         String idToken = googleCredential.getGoogleIdToken();
@@ -152,3 +203,4 @@ public class MainActivity extends AppCompatActivity {
         return password.length() > 0;
     }
 }
+
